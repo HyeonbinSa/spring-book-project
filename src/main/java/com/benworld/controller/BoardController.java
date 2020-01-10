85e87aab29131362afdce3848f4d008ef19c5ccd
사현빈 <sahyeonbin@sahyeonbin-ui-MacBookAir.local>
2020-01-10 16:14:51 +0900

    Spring CRUD 수정, 삭제 기능 완료

--- src/main/java/com/benworld/controller/BoardController.java
+++ src/main/java/com/benworld/controller/BoardController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.benworld.domain.BoardVO;
 import com.benworld.service.BoardService;
@@ -26,13 +28,13 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String registerPOST(BoardVO vo, Model model) throws Exception {
+	public String registerPOST(BoardVO vo, RedirectAttributes rttr) throws Exception {
 		logger.info("register Post .............");
 		logger.info(vo.toString());
 		
 		service.regist(vo);
 		
-		model.addAttribute("result", "success");
+		rttr.addFlashAttribute("msg", "SUCCESS");
 		
 		return "redirect:/board/listAll";
 	}
@@ -40,7 +42,35 @@ public class BoardController {
 	@RequestMapping(value="/listAll", method= RequestMethod.GET)
 	public void listAll(Model model)throws Exception{
 		logger.info("show all list.........");
+		
+		model.addAttribute("list", service.listAll());
 	}
 	
-
+	@RequestMapping(value="/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+		model.addAttribute(service.read(bno));
+	}
+	@RequestMapping(value="/remove", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception{
+		service.remove(bno);
+		
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value="/modify", method=RequestMethod.GET)
+	public void modifyGET(int bno, Model model)throws Exception{
+		model.addAttribute(service.read(bno));
+	}
+	@RequestMapping(value="/modify", method=RequestMethod.POST)
+	public String modifyPOST(BoardVO vo, RedirectAttributes rttr) throws Exception {
+		logger.info("mod post.........");
+		
+		service.modify(vo);
+		
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+	}
 }
--- src/main/webapp/WEB-INF/views/board/listAll.jsp
+++ src/main/webapp/WEB-INF/views/board/listAll.jsp
@@ -2,9 +2,17 @@
 <%@ page session="false" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"  %>
+<%@ page session="false" %>        
 
 <%@ include file="/WEB-INF/views/include/header.jsp" %>
-
+<<script type="text/javascript">
+ var result = '${msg}';
+ if(result == 'SUCCESS'){
+	 alert("처리가 완료되었습니다.");
+ }
+</script>
 <table class="table table-bordered">
 	<tr>
 	<th style="width: 10px">BNO</th>
@@ -13,6 +21,15 @@
 	<th>RegDate</th>
 	<th style="witdh:40px">ViewCnt</th>
 	</tr>
+	<c:forEach items="${list}" var="boardVO"> 
+		<tr>
+			<td>${boardVO.bno }</td>
+			<td><a href='/board/read?bno=${boardVO.bno }'>${boardVO.title }</a></td>
+			<td>${boardVO.content }</td>
+			<td>${boardVO.writer }</td>
+			<td><fmt:formatDate pattern="yyyy-MM-dd HH:mm" value="${boardVO.regdate }"/></td>
+			<td><span class="badge bg-red">${boardVO.viewcnt}</span></td>
+	</c:forEach>
 </table>
 <%@ include file="/WEB-INF/views/include/footer.jsp" %>
 
